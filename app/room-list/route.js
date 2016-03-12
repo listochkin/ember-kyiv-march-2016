3b1ed7607ff732d00d8a4263d1a172404f7f2ea6
Andrey Listochkin <andrey.listochkin@gmail.com>
2016-03-12 12:22:05 +0200

    simple navigation between apps

--- app/room-list/route.js
+++ app/room-list/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model () {
+    return Ember.RSVP.resolve([
+      { id: 11, name: 'Welcome' },
+      { id: 12, name: 'Need Help' },
+      { id: 13, name: 'Offtopic' }
+    ]);
+  }
 });
